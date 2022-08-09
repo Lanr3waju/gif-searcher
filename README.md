c73217a8b0448b9a5bb3406246be884ba8614752
Abass Olanrewaju <Abassolanrewaju111@gmail.com>
2022-08-10 00:29:31 +0100

    Refactor code to prevent page reload

---
+++ README.md
@@ -0,0 +1,76 @@
+# Gif Searcher
+
+> This is a task on API's, 
+
+![screenshot](./src/img/awesome-books.png)
+
+This is an app that accepts keyword input and searches for the gif that matches that keyword on giphy's API. 
+
+## Built With
+
+- HTML
+
+
+- Java Script
+- Implemented DOM manipulation in various parts of the project to make page dynamic.
+- JS `Objects`, `Arrays`, & `Object Arrays` to store data of similar items.
+- Implement the use of Arrow Functions `=>` and `{destructure}` methods to match the modern conventions.
+- Fetch API.
+- Implement the use of `eventListeners` to make 'add' & 'remove' buttons functional.
+- Implement the use of `array.find()` & `parseInt` to locate the specific books to remove when the appropriate button is clicked.
+- Promise and its methods
+- The `node.localStorage` `set()` & `get()` methods were used to store and retrieve user inouts.
+
+- CSS
+  - Pseudo-selectors (hover; to add hover effect to certain elements)
+  - Pseudo-elements
+  - CSS position property
+  - `Flexbox`
+  - Transition property
+  - CSS background property
+  - CSS animation
+  - `Box-Shadow` Properties.
+
+## Live Demo
+
+[Live Demo Link](https://e-bookstore-pwa.netlify.app/)
+
+### Development (Running locally)
+
+- Install Dependencies
+
+```bash
+npm install
+```
+
+## Style Guides
+
+- [CSS Style Guide](http://udacity.github.io/frontend-nanodegree-styleguide/css.html)
+- [HTML Style Guide](http://udacity.github.io/frontend-nanodegree-styleguide/index.html)
+- [JavaScript Style Guide](http://udacity.github.io/frontend-nanodegree-styleguide/javascript.html)
+- [Git Style Guide](https://udacity.github.io/git-styleguide/)
+
+## 👤 Author
+
+- Github: [@Lanr3waju]<https://github.com/Lanr3waju>)
+- LinkedIn: [@Abass Olanrewaju](https://www.linkedin.com/in/abass-olanrewaju-abdul-wasi-53883422a/)
+- Twitter: [@abdul-wasi-img](https://twitter.com/abdul_wasi_img)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- I appreciate the efforts of my mentors for their constatnt guidance and contribtution.
+- [@bolah2009](http://github.com/bolah2009)
+
+## 📝 License
+
+[MIT licensed](./LICENSE).
--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
-const img = document.querySelector('#gif-img');
-const search = document.querySelector('form');
-const inp = document.querySelector('#text-inp');
-const spinner = document.querySelector('#spinner');
+const img = document.querySelector('#gif-img')
+const search = document.querySelector('form')
+const spinner = document.querySelector('#spinner')
+const errorPrompt = document.querySelector('#error-prompt')
+const closeError = document.querySelector('#close')
 
 const assignImgUrl = (el, url) => {
   el.src = url
-};
+}
 
-const setGifToSearch = () => {
+const addElClass = (el, className) => {
+  el.classList.add(className)
+}
+
+const removeElClass = (el, className) => {
+  el.classList.remove(className)
+}
+
+const closeErrorPrompt = () => {
+  addElClass(errorPrompt, 'none');
+  removeElClass(img, 'none');
+}
+
+const fetchGif = event => {
+  event.preventDefault()
+  const inp = document.querySelector('#text-inp')
+  addElClass(img, 'none')
+  removeElClass(spinner, 'none')
   const gifToSearch = inp.value
   localStorage.setItem('val', gifToSearch)
-};
-
-const addElToClass = (el, className) => {
-  el.classList.add(className)
-};
-
-const fetchRequestTimeout = setTimeout(() => {
-  assignImgUrl(img, './img/256x256.png')
-  alert('Oops! we ran into a problem, Try Again!')
-}, 7000);
-
-const fetchGif = () => {
+  
   fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=VtmQZFpxMbOczLM5Vw5zU8sqZ13H0VX7&s=${
+    `https://api.giphy.com/v1/gifs/translate?api_key=qrMWKOWofYZsz4eOSCm92mboLFxOO8d4&s=${
       localStorage.getItem('val') ? localStorage.getItem('val') : 'cat'
     }`,
     { mode: 'cors' }
@@ -32,22 +39,24 @@ const fetchGif = () => {
       return response.json()
     })
     .then(response => {
-      const girUrl = response.data.images.original.url
+      const {data: {images: {fixed_height_small: {url}}}} = response;
+      const girUrl = url
       assignImgUrl(img, girUrl)
-      clearTimeout(fetchRequestTimeout)
-      addElToClass(spinner, 'none')
+      removeElClass(img, 'none')
+      addElClass(spinner, 'none')
     })
     .catch(e => {
-      assignImgUrl(img, './img/256x256.png');
-      clearTimeout(fetchRequestTimeout);
-      addElToClass(spinner, 'none');
-      alert(e);
+      assignImgUrl(img, './img/128x128.png')
+      addElClass(spinner, 'none')
+      removeElClass(errorPrompt, 'none')
     })
-};
+}
 
 const startApp = () => {
-  fetchGif()
-  search.addEventListener('submit', setGifToSearch)
-};
+  assignImgUrl(img, './img/128x128.png')
+  addElClass(spinner, 'none')
+  search.addEventListener('submit', fetchGif)
+  closeError.addEventListener('click', closeErrorPrompt)
+}
 
-startApp();
+startApp()
